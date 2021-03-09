e947da7b445da6a9ef6f339f4ec98c5513d1f2e8
Peter <petergajgau@gmail.com>
2021-03-09 14:57:56 +0100

    a

--- ProjektAngular/new-project/src/app/app-routing.module.ts
+++ ProjektAngular/new-project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'watchlist', component: WatchlistComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'home', component: HomeComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ProjektAngular/new-project/src/app/app.module.ts
+++ ProjektAngular/new-project/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    HomeComponent,
+    SearchComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
